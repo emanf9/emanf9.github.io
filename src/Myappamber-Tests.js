a8c3582167d82e801cac532447e933434b2f7a09
emanf9 <emanf2002@gmail.com>
2015-12-18 21:30:05 -0600

    Removing Everything and Adding Amber Installations
    I could turn this into a proper blog

---
+++ src/Myappamber-Tests.js
@@ -0,0 +1,9 @@
+define("amber-myappamber/Myappamber-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){
+var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
+var smalltalk=$core,_st=$recv,globals=$globals;
+$core.addPackage('Myappamber-Tests');
+$core.packages["Myappamber-Tests"].transport = {"type":"amd","amdNamespace":"amber-myappamber"};
+
+$core.addClass('MyappamberTest', $globals.TestCase, [], 'Myappamber-Tests');
+
+});
