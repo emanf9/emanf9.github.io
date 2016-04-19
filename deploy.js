d39e7d06ac3f4fa4d28fc9b543121c083214a9cc
Ethan Falck <emanf2002@gmail.com>
2016-04-19 15:26:45 -0500

    Delete deploy.js

--- deploy.js
+++
@@ -1,8 +0,0 @@
-define([
-    'amber/deploy',
-    // --- packages to be deployed begin here ---
-    'amber-myappamber/Myappamber'
-    // --- packages to be deployed end here ---
-], function (amber) {
-    return amber;
-});
